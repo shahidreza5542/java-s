56f64471fa891591f5b5f0d8864410133e90b5e1
shahidreza <shahidreza660@gmail.com>
2025-07-30 08:30:03 +0530

    new files 1

---
+++ 01-basic/03-conversion-opretion.js
@@ -0,0 +1,30 @@
+let score = "false"
+
+//  console.log(typeof (score));
+//   console.log(typeof score);
+ 
+  let valueInNumber = Number(score)
+
+//   console.log(typeof valueInNumber);
+//     console.log(valueInNumber);
+ 
+
+    // "33" ==> 33
+    // "33abc" ==> nan
+    // true ==> 1; false ==> 0
+
+    let isLoggedin = "shahid"
+
+    let booleanisLoggedin = Boolean(isLoggedin)
+
+    console.log(booleanisLoggedin);
+    
+    // 1 ==> true; 0 ==> false
+    // "" ==> false
+    // "shahid" ==> true
+
+    let someNumber = 33
+    let stringNumber = String(someNumber);
+    console.log(stringNumber);
+    console.log(typeof stringNumber);
+    
