dbd86009fb489b4e37d2885aed3e719ce13e2fb6
shahidreza <shahidreza660@gmail.com>
2025-07-30 08:12:50 +0530

    new

---
+++ 01-basic/02-datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+// alert("hello") 
+
+console.log(3 + 3); 
+
+let name = "shahid"
+let age = 16
+let isLoggedin = false
+let state;
+
+
+// number ==> 2 to power 53
+// bigint
+// string ==> ""
+// boolean ==> truw/false
+// null ==> standalone value
+// undefined ==>
+// symbol ==> unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
