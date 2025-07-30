cc20582ece0c472e19ab42207f21f4df2f22f6ad
shahidreza <shahidreza660@gmail.com>
2025-07-30 07:53:08 +0530

    new files

---
+++ 01-basic/01-var.js
@@ -0,0 +1,16 @@
+const accountId = 14445656
+let accountemail = "shahid@gmail"
+var accountPassword = "12444"
+accountCity = "bihar"
+
+
+
+// accountId = 2 /not allowed
+accountemail= "sa@gmail.com"
+accountPassword = "shahu"
+accountCity = "hyd"
+let accountState;
+
+// not to use var because of issue in block scope and functional scope
+
+console.table([accountId,accountPassword,accountCity,accountemail,accountState]);
