16d6249ea6e834d4d04379b1f0966f4061ed1ba6
shahidreza <shahidreza660@gmail.com>
2025-07-31 06:39:25 +0530

    summary

---
+++ 01-basic/05-dt-summary.js
@@ -0,0 +1,37 @@
+// primitive
+
+// 7 types ; string, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.1
+
+const isLoggedin = false
+const outsideTemp = null
+let useremail;
+
+const id = Symbol(5000242)
+const anotherId = Symbol("123")
+
+console.log(id == anotherId);
+
+const bigNumber = 1446844757547544n
+
+// Reference (non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "ironman", "doga"];
+
+let myObj = {
+    name: "shahid",
+    age: 17,
+}
+
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof anotherId)
+
