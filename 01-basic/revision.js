29a591d896902682761e352871f983db4954346f
shahidreza <shahidreza660@gmail.com>
2025-08-02 07:46:54 +0530

    number

---
+++ 01-basic/revision.js
@@ -0,0 +1,52 @@
+// console.log("hello world");
+
+// const Id = "1039245465"
+// let name = "shahid"
+// const score = 500
+
+// name = "jahid"
+
+// console.table([Id, name, score])
+
+
+// Id = 24687 const ko change nhi kiya ja sakta
+
+// primitive datatype
+
+// string, number, boolearn, null, undefaind, bigent, symbol
+
+let name = "shahid"
+let score = 50 
+let isLoggedin = true 
+let address = null
+let email; 
+
+// console.table([name, score, isLoggedin, address, email])
+
+// console.log(2 + 2);
+// console.log(2 + "2");
+// console.log(2 + 2 + "2");
+// console.log("2" + 2 + 2);
+
+let url = "https://shhaid.com/shah%20sjhf"
+// console.log(url.replace('%20'));
+
+const gameName = "shahid"
+const repoCount = 50
+
+// let newstring = new String name('shahid-c')
+
+let heros = [hero1, hero2, hero3]
+
+let myObj = {
+    name: "shahid",
+    age: 50
+}
+
+let myFunction = function(){
+    console.log("hello world")
+
+}
+
+
+
