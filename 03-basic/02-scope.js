2428d810fa291d4dc3eaf169c5fa682fcae863c9
shahidreza <shahidreza660@gmail.com>
2025-08-06 10:08:40 +0530

    scope

--- 03-basic/02-scope.js
+++ 03-basic/02-scope.js
@@ -6,12 +6,59 @@ if (true) {
 
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "shahid"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+}
+
+// one() 
+
+if (true) {
+    const username = "shahid"
+    if (username === "shahid"){
+        const website = "youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++ interesting ++++++
+
+addone(2)
+function addone (num){
+    return num + 1
+}
+
+
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
+
