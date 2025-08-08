b477cfbc62fbc1fb3b5e55cd67a7c6f70c0cfd8a
shahidreza <shahidreza660@gmail.com>
2025-08-08 20:18:12 +0530

    swich

---
+++ 04-control-flow/01.js
@@ -0,0 +1,63 @@
+// if
+const isUerLoggedin = true
+const temp = 43
+
+
+// if (temp === 44) {
+//     console.log("less then 50");
+
+// } else {
+//     console.log("temp is greater than 50");
+// }
+
+
+
+
+// <, >, <=,==, !=, ===, !==
+
+// const score = 300
+
+// if (score > 200) {
+//     let power = "fly"
+//     console.log(`user power: ${power}`);
+
+// }
+
+// console.log(`user power: ${power}`);
+
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test");
+
+// if (balance < 500) {
+//      console.log("less than 500");
+
+// } else if (balance < 750) {
+//     console.log("less than 750");
+
+// } else if (balance < 850) {
+//     console.log("less than 850");
+
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+
+const userLoggedin = true
+const debitcard = true
+const loggedinfromgoogle = false
+const loggedinfromemail = true
+
+if (userLoggedin && debitcard && 2 == 3) {
+    console.log("allow to buy course");
+
+}
+
+if (loggedinfromgoogle || loggedinfromemail) {
+    console.log("user logged in");
+    
+    
+}
---
+++ 04-control-flow/02.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "apt"
+
+switch (month) {
+    case 1:
+        console.log("jan");
+
+        break;
+    case 2:
+        console.log("feb");
+
+        break;
+
+    case 3:
+        console.log("march");
+
+        break;
+    case 4:
+        console.log("apr");
+
+        break;
+
+    default:
+        console.log("def");
+        
+        break;
+}
